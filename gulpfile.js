356619d215a2a4dbb89fd07ba19b2a46eb712d4f
Daniel M. Sánchez <dmsanchez86@misena.edu.co>
2016-06-10 17:04:28 -0500

    create gulp file, add new dependencies to package.json, minified all files css and js

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    concatJs = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    uglify = require('gulp-uglify');
+    
+gulp.task('css', function (){
+  gulp.src('stylesheets/*.css')
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify('task css ended'));
+});
+
+gulp.task('js', function(){
+  gulp.src('js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
+  .pipe(notify('task js ended'));
+});
+
+gulp.task('default',['css', 'js'], function(){
+  gulp.src('lib/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/lib'))
+  .pipe(notify('task lib js ended'));
+});
