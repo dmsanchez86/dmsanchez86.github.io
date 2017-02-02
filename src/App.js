f344f30c72fe692bb56f8f3fd99109f7b1b1679d
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2017-02-02 18:22:33 -0500

    se organizo el componente de App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: 'My initial state'
-    }
-  }
-
-  update(event){
-    this.setState({
-      text: event.target.value
-    });
-  }
-
-  clickButton(){
-    alert(this.state.text);
-  }
 
   render() {
+    const itemsM = [
+      {
+        id: 1,
+        name: 'Home',
+        href: '#home',
+        classItem: 'active',
+        favicon: 'fa fa-home'
+      },{
+        id: 2,
+        name: 'Projects',
+        href: '#projects',
+        classItem: '',
+        favicon: 'fa fa-gamepad'
+      },{
+        id: 3,
+        name: 'Portafolio',
+        href: '#portafolio',
+        classItem: '',
+        favicon: 'fa fa-flask'
+      },{
+        id: 4,
+        name: 'Contact',
+        href: '#contact',
+        classItem: '',
+        favicon: 'fa fa-mobile'
+      }
+    ];
+
     return (
       <main>
-        <NavBar />
+        <NavBar items={itemsM} />
       </main>
     );
   }
