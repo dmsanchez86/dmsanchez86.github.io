c4da85b23cc6cfa6b8ff413e1287b18860137316
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 19:59:44 -0500

    componente base para las herramientas

---
+++ src/app/components/tools/tools.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tools',
+  template: `
+    <div class="tools">
+      <div *ngIf="preview" (click)="previewF()" class="tool preview" title-ref="Preview">
+        <i class="fa fa-desktop"></i>
+      </div>
+      <div *ngIf="code" (click)="codeF()" class="tool code" title-ref="Check code on GitHub ">
+        <i class="fa fa-code"></i>
+      </div>
+      <div *ngIf="download" (click)="downloadF()" class="tool download" title-ref="Download">
+        <i class="fa fa-download"></i>
+      </div>
+    </div>
+  `
+})
+export class ToolsComponent {
+  @Input() preview: true;
+  @Input() code: false;
+  @Input() download: false;
+
+  previewF(){ console.log('preview'); }
+
+  codeF(){ console.log('code'); }
+
+  downloadF(){ console.log('download'); }
+
+}
