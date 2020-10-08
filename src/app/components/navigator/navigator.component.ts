c22b082bec1ba02ddb8fb9b5afaaea3d73e79415
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 20:31:53 -0500

    ajustes en el navigator para poder renderizar el menu dinamicamente

--- src/app/components/navigator/navigator.component.ts
+++ src/app/components/navigator/navigator.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 declare const $;
 
+interface MenuI {
+  name: string;
+  icon: string;
+  href: string;
+}
+
 @Component({
   selector: 'app-navigator',
-  templateUrl: './navigator.component.html'
+  templateUrl: './navigator.component.html',
 })
-export class NavigatorComponent implements OnInit {
+export class NavigatorComponent {
+  menus: MenuI[] = [
+    {
+      name: 'Home',
+      href: '#home',
+      icon: 'home',
+    },
+    {
+      name: 'Projects',
+      href: '#projects',
+      icon: 'gamepad',
+    },
+    {
+      name: 'Portafolio',
+      href: '#portafolio',
+      icon: 'flask',
+    },
+    {
+      name: 'Contact',
+      href: '#contact',
+      icon: 'mobile',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  buttonMenu(){
+  buttonMenu() {
     $('body').addClass('menu');
     $('#menu,.overlay-menu').addClass('open');
     $(this).fadeOut('1000');
   }
-
 }
