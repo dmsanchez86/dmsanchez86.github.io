cdfdfd9055b0a81829b23eefbc264e87d720dfd1
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 21:53:48 -0500

    ajuste en link del logo

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-logo',
   template: `
     <div class="logo" tabindex="1">
-      <a href="#home" title="dmsanchez86 WebSite">
+      <a [routerLink]="['/']" routerLinkActive="active"  title="dmsanchez86 WebSite">
         <svg xmlns="http://www.w3.org/2000/svg">
           <circle cx="29px" cy="25px" r="23px"></circle>
           <text x="-1" y="35" dx="0" dy="0">D</text>
