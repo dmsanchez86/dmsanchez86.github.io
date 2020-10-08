50b71520d248fb203a254fbd28736aa7b97e6a68
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 20:19:22 -0500

    reducer para las collaborations

---
+++ src/app/store/reducers/collaborations.ts
@@ -0,0 +1,13 @@
+import { createReducer, on } from '@ngrx/store';
+import { loadCollaborations } from '../actions/collaborations';
+
+const initialState = [];
+
+const _collaborationsReducer = createReducer(
+  initialState,
+  on(loadCollaborations, (state, action) => ([ ...state, action.payload ])),
+);
+
+export function collaborationsReducer(state, action) {
+  return _collaborationsReducer(state, action);
+}
