7353dc75d3c0534fcc6edd40e009e1b4cf32a7c8
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 22:48:02 -0500

    store para las collaborations

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { ProjectI } from 'src/app/store/reducers/projects/dataProjects';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent implements OnInit {
-  constructor() {}
+  projects: Observable<ProjectI[]> = this.store.select('collaborations');
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {}
 }
