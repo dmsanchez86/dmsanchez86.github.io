de2bc8fbca1bbeca13a20b3d475011156e26a22a
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 20:19:11 -0500

    reducer root

---
+++ src/app/store/reducers/index.ts
@@ -0,0 +1,10 @@
+import { ActionReducerMap } from '@ngrx/store';
+import { AppState } from '..';
+
+import { projectsReducer } from './projects';
+import { collaborationsReducer } from './collaborations';
+
+export const reducers: ActionReducerMap<AppState> = {
+  projects: projectsReducer,
+  collaborations: collaborationsReducer
+};
