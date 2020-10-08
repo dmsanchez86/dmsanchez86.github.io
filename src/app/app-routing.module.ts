f64cd9f46389fd23abf98a771746ce5ea00a55f9
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 21:04:15 -0500

    ruta para el portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'portafolio', loadChildren: () => import('./pages/portafolio/portafolio.module').then(m => m.PortafolioModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
