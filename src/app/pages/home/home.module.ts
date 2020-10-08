29daa5ffb80ce3aca6b3f0f4b7d45956592032a8
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-07 21:36:42 -0500

    se limpio de componentes el home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,24 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { AtomLoaderComponent } from 'src/app/components/atom-loader/atom-loader.component';
-import { NavigatorComponent } from 'src/app/components/navigator/navigator.component';
-import { PreviewComponent } from 'src/app/components/preview/preview.component';
-import { PopupComponent } from 'src/app/components/popup/popup.component';
-import { ToolsComponent } from 'src/app/components/tools/tools.component';
-import { LogoComponent } from 'src/app/components/logo/logo.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    AtomLoaderComponent,
-    NavigatorComponent,
-    PreviewComponent,
-    PopupComponent,
-    ToolsComponent,
-    LogoComponent,
-  ],
-  imports: [CommonModule, HomeRoutingModule],
+  declarations: [ HomeComponent ],
+  imports: [CommonModule, HomeRoutingModule, ComponentsModule],
 })
 export class HomeModule {}
