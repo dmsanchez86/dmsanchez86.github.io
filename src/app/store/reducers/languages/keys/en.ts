9d7d124caaef03b2350aa7d9c00be7e50dc7705e
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-08 22:25:46 -0500

    nuevos datos para el perfil

--- src/app/store/reducers/languages/keys/en.ts
+++ src/app/store/reducers/languages/keys/en.ts
@@ -1,7 +1,7 @@
 export const EN = {
   ref: 'en',
   logo: {
-    title: 'dmsanchez86 Website'
+    title: 'dmsanchez86 Website',
   },
   nav: {
     home: 'Home',
@@ -10,4 +10,24 @@ export const EN = {
     contact: 'Contact',
     hobbies: 'Hobbies',
   },
-}
+  profile: {
+    nick: 'dmsanchez86',
+    phone: '+(57) 320 376 8421',
+    name: 'Daniel Mauricio Sanchez Avila',
+    biography: `Frontend Developer. I'm from Manizales Caldas in Colombia, currently live in Cali, Valle del Cauca.`,
+    social: {
+      share: {
+        github: 'On Github',
+        twitter: 'On Twitter',
+        facebook: 'On Facebook',
+        google: 'On Google+',
+        linkedin: 'On LinkedIn',
+        instagram: 'On instagram',
+        email: 'Send Email to',
+        whatsapp: '+(57) 320 376 8421',
+        freecodecamp: 'On Free Code Camp',
+        codepen: 'On Codepen',
+      },
+    },
+  },
+};
--- src/app/store/reducers/languages/keys/es.ts
+++ src/app/store/reducers/languages/keys/es.ts
@@ -10,4 +10,24 @@ export const ES = {
     contact: 'Contacto',
     hobbies: 'Pasatiempos',
   },
+  profile: {
+    nick: 'dmsanchez86',
+    phone: '+(57) 320 376 8421',
+    name: 'Daniel Mauricio Sanchez',
+    biography: `Desarrollador FrontEnd. Soy de Colombia, del departamento de Caldas, Nací en la ciudad de Manizales. Actualmente vivo en la ciudad de Cali, valle del Cauca.`,
+    social: {
+      share: {
+        github: 'En Github',
+        twitter: 'En Twitter',
+        facebook: 'En Facebook',
+        google: 'En Google+',
+        linkedin: 'En LinkedIn',
+        instagram: 'En instagram',
+        email: 'Enviar correo electrónico a',
+        whatsapp: '+(57) 320 376 8421',
+        freecodecamp: 'En Free Code Camp',
+        codepen: 'En Codepen',
+      },
+    },
+  },
 };
