4feeef9e0ced1cc8fbf3095dc07644a35b5badd1
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-08 21:43:03 -0500

    logicapara cambiar el idioma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { GlobalService } from './services/global.service';
+import { AppState } from './store';
+import { changeLanguage } from './store/actions/language';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +13,9 @@ import { GlobalService } from './services/global.service';
 export class AppComponent {
   title = 'pageAngular';
 
-  constructor(private global: GlobalService){
+  language: Observable<any> = this.store.select(state => state.language);
+
+  constructor(private global: GlobalService, private store: Store<AppState>){
     setTimeout(() => {
       let atomLoader: HTMLElement = document.querySelector('#atomLoader');
       atomLoader.style.display = 'none';
@@ -18,5 +24,9 @@ export class AppComponent {
 
   cerrarMenu(){
     this.global.cerrarMenu();
+  }
+
+  cambiarLenguaje(key){
+    this.store.dispatch(changeLanguage({ key }));
   }
 }
