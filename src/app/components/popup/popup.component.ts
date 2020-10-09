2edb96f11e3aaee0ec8103b35bbd87fbb47a3ceb
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-08 22:26:16 -0500

    validacion con store en popup

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html'
 })
 export class PopupComponent implements OnInit {
+  language: Observable<any> = this.store.select(state => state.language.current.profile);
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
   }
