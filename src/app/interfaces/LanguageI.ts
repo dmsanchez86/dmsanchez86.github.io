b734ed46e5f1bc8fed0c547a6ebd198ad8ab6787
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-09 22:06:12 -0500

    interfaz bae para el diccionario

---
+++ src/app/interfaces/LanguageI.ts
@@ -0,0 +1,80 @@
+export interface LanguageItemI {
+  ref: string;
+  title: string;
+  logo: {
+    title: string;
+  };
+  nav: {
+    home: string;
+    projects: string;
+    portafolio: string;
+    contact: string;
+    hobbies: string;
+  };
+  profile: {
+    nick: string;
+    phone: string;
+    name: string;
+    biography: string;
+    hobbies_bio: string;
+    social: {
+      share: {
+        github: string;
+        twitter: string;
+        facebook: string;
+        google: string;
+        linkedin: string;
+        instagram: string;
+        email: string;
+        whatsapp: string;
+        freecodecamp: string;
+        codepen: string;
+      };
+    };
+  };
+  global: {
+    tweets: string;
+  };
+  tools: {
+    preview: string;
+    check: string;
+    download: string;
+  };
+  projects: {
+    football: string;
+    atom: string;
+    form: string;
+    jurisquiz_app: string;
+    quotes: string;
+    ball: string;
+    weather: string;
+    slide: string;
+    clock: string;
+    cellphone: string;
+    cube: string;
+    lists: string;
+    geometry_cube: string;
+    jurisquiz_web: string;
+    arthritis: string;
+    angular_template: string;
+    mecaut: string;
+  };
+  portafolio: {
+    go: string;
+    between_pines: string;
+    oral_center: string;
+    conexiones: string;
+    zeus_web: string;
+    santodomingo: string;
+    antorcha: string;
+    raking: string;
+    jurisquiz: string;
+    therapy: string;
+    zopp: string;
+    buffalo: string;
+    easynet: string;
+    cliniweb: string;
+    hospital: string;
+    finca: string;
+  };
+}
