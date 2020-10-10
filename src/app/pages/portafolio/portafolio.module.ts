555a388f61045c378000877c29b6c70ee66e227d
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-10 00:45:44 -0500

    nueva ruta para el portafolio

--- src/app/pages/portafolio/portafolio.module.ts
+++ src/app/pages/portafolio/portafolio.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PortafolioComponent } from './portafolio.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [{
-  path: '', component: PortafolioComponent
-}]
+const routes: Routes = [
+  { path: '', component: PortafolioComponent },
+  { path: ':id', component: DetailComponent },
+];
 
 @NgModule({
-  declarations: [PortafolioComponent],
+  declarations: [PortafolioComponent, DetailComponent],
   imports: [
     CommonModule,
     ComponentsModule,
