e311c57845d8c6d1c00e7ce36cbc9806ec22c49a
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-09 23:15:10 -0500

    soporte para el lenguaje en las tools components

--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -1,19 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LanguageItemToolsI } from 'src/app/interfaces/LanguageI';
+import { AppState } from 'src/app/store';
 
 @Component({
   selector: 'app-tools',
   template: `
     <div class="tools">
-      <div *ngIf="preview" (click)="previewF()" class="tool preview" title-ref="Preview">
+      <div *ngIf="preview" (click)="previewF()" class="tool preview" [attr.title-ref]="(language | async).preview">
         <i class="fa fa-desktop"></i>
       </div>
-      <div *ngIf="code" (click)="codeF()" class="tool code" title-ref="Check code on GitHub ">
+      <div *ngIf="code" (click)="codeF()" class="tool code" [attr.title-ref]="(language | async).check">
         <i class="fa fa-code"></i>
       </div>
-      <div *ngIf="download" (click)="downloadF()" class="tool download" title-ref="Download">
+      <div *ngIf="download" (click)="downloadF()" class="tool download" [attr.title-ref]="(language | async).preview">
         <i class="fa fa-download"></i>
       </div>
-      <div *ngIf="app" (click)="codeF()" class="tool code" title-ref="Install App">
+      <div *ngIf="app" (click)="codeF()" class="tool code" [attr.title-ref]="(language | async).download">
         <i class="fa fa-android"></i>
       </div>
     </div>
@@ -24,6 +28,10 @@ export class ToolsComponent {
   @Input() code: false;
   @Input() download: false;
   @Input() app: false;
+
+  language: Observable<LanguageItemToolsI> = this.store.select(state => state.language.current.tools);
+
+  constructor(private store: Store<AppState>){}
 
   previewF(){ console.log('preview'); }
 
