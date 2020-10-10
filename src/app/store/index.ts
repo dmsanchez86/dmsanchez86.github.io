5e5b8c241c3a844d33c62099e401c66c969cd606
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-09 22:49:14 -0500

    se agregaron interfaces a todas las porciones del store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,6 +1,24 @@
+import { LanguageItemI } from '../interfaces/LanguageI';
+import { MenuItemI } from '../interfaces/MenuItemI';
+import { ProjectItemI } from '../interfaces/ProjectItemI';
+
 export interface AppState {
-  projects: any;
-  collaborations: any;
-  menu: any;
-  language: any;
+  projects: AppStateProjectsI;
+  collaborations: AppStateCollaborationsI;
+  menu: AppStateMenu;
+  language: AppStateLanguaje;
+}
+export interface AppStateProjectsI {
+  data: ProjectItemI[]
+}
+export interface AppStateCollaborationsI {
+  data: ProjectItemI[]
+}
+export interface AppStateMenu {
+  data: MenuItemI[];
+}
+export interface AppStateLanguaje {
+  es: LanguageItemI,
+  en: LanguageItemI,
+  current: LanguageItemI
 }
