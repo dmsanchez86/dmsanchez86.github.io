47296ebd129962a675ec90d6af7f727cf8b48928
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-10 18:28:56 -0500

    validacion en el componente de preview page para maximizar la pantalla

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
 import { ProjectItemI } from 'src/app/interfaces/ProjectItemI';
+import { AppState } from 'src/app/store';
 
 @Component({
   selector: 'app-preview',
-  templateUrl: './preview.component.html'
+  templateUrl: './preview.component.html',
 })
 export class PreviewComponent {
   @Input() project: ProjectItemI;
+  validationLoad: boolean = false;
+  validationFullscreen: boolean = false;
 
-  constructor() { }
+  constructor(private store: Store<AppState>, public sanitizer: DomSanitizer) {}
+
+  loadIframe($event) {
+    // console.log($event);
+    // console.warn('carga');
+    // this.validationLoad = true;
+  }
+
+  setFullscreen($event) {
+    this.validationFullscreen = !this.validationFullscreen;
+  }
 }
