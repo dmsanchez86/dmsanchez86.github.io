6cd6722ec598b197d34e048d9178fb4e0167e2b1
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-09 21:48:41 -0500

    soporte base para mostrar segun el idioma el texto del hobbie

--- src/app/pages/hobbies/hobbies.component.ts
+++ src/app/pages/hobbies/hobbies.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
 
 @Component({
   selector: 'app-hobbies',
   templateUrl: './hobbies.component.html',
 })
 export class HobbiesComponent implements OnInit {
-  constructor() {}
+  language: Observable<any> = this.store.select(state => state.language.current.profile);
+  constructor(private store: Store<AppState>) {}
 
   ngOnDestroy() {
     document.body.classList.remove('contact');
