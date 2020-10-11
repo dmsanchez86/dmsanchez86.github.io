9b5ee11023084f872e15a5802b6a495f8c6e0726
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-10 21:23:05 -0500

    archivo con la data del menu

---
+++ src/app/store/reducers/menu/dataMenu.ts
@@ -0,0 +1,34 @@
+import { MenuItemI } from 'src/app/interfaces/MenuItemI';
+
+export const dataMenu: MenuItemI[] = [
+  {
+    name: 'Home',
+    href: '/',
+    key: 'home',
+    icon: 'home',
+  },
+  {
+    name: 'Projects',
+    href: '/projects',
+    key: 'projects',
+    icon: 'gamepad',
+  },
+  {
+    name: 'Portafolio',
+    href: '/portafolio',
+    key: 'portafolio',
+    icon: 'flask',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+    key: 'contact',
+    icon: 'mobile',
+  },
+  {
+    name: 'Hobbies',
+    href: '/hobbies',
+    key: 'hobbies',
+    icon: 'circle',
+  },
+];
