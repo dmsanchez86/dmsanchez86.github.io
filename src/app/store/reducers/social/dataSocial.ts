6a1fe100ea0f0783c1808b50b01f13ec1edb81cf
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-10 21:22:31 -0500

    archivo con los datos de las redes sociales

---
+++ src/app/store/reducers/social/dataSocial.ts
@@ -0,0 +1,74 @@
+import { SocialItemI } from 'src/app/interfaces/SocialItemI';
+
+export const dataSocial: SocialItemI[] = [
+  {
+    name: 'http://github.com/dmsanchez86',
+    href: '/',
+    key: 'github',
+    icon: 'fa fa-github',
+    type: ''
+  },
+  {
+    name: 'Twitter',
+    href: 'http://twitter.com/dmsanchez86',
+    key: 'twitter',
+    icon: 'fa fa-twitter-square',
+    type: ''
+  },
+  {
+    name: 'Facebook',
+    href: 'http://facebook.com/dmsanchez86',
+    key: 'facebook',
+    icon: 'fa fa-facebook-square',
+    type: ''
+  },
+  {
+    name: 'Google+',
+    href: 'https://plus.google.com/109279201439651083225',
+    key: 'google',
+    icon: 'fa fa-google-plus-square',
+    type: ''
+  },
+  {
+    name: 'Linkedin',
+    href: 'https://co.linkedin.com/in/dmsanchez86',
+    key: 'linkedin',
+    icon: 'fa fa-linkedin-square',
+    type: ''
+  },
+  {
+    name: 'Instagram',
+    href: 'http://instagram.com/danysanchez7',
+    key: 'instagram',
+    icon: 'fa fa-instagram',
+    type: ''
+  },
+  {
+    name: 'Email',
+    href: 'mailto:',
+    key: 'email',
+    icon: 'fa fa-envelope-o',
+    type: 'email'
+  },
+  {
+    name: 'WhatsApp',
+    href: 'https://wa.me/',
+    key: 'whatsapp',
+    icon: 'fa fa-whatsapp',
+    type: 'tel'
+  },
+  {
+    name: 'Free Code Camp',
+    href: 'http://freecodecamp.com/dmsanchez86',
+    key: 'freecodecamp',
+    icon: 'fa fa-fire',
+    type: ''
+  },
+  {
+    name: 'CodePen',
+    href: 'http://codepen.io/dmsanchez86',
+    key: 'codepen',
+    icon: 'fa fa-codepen',
+    type: ''
+  },
+];
