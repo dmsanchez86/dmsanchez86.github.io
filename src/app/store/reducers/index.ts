0abf002fdd2ebd9a7a48902cca8fcadb48352d0b
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-10 21:25:57 -0500

    reducer y porcion del estado para lo social

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -3,12 +3,14 @@ import { AppState } from '..';
 
 import { projectsReducer } from './projects/';
 import { portafolioReducer } from './portafolio/';
-import { menuReducer } from './menu';
+import { menuReducer } from './menu/';
 import { languageReducer } from './languages';
+import { socialReducer } from './social';
 
 export const reducers: ActionReducerMap<AppState> = {
   projects: projectsReducer,
   portafolio: portafolioReducer,
   menu: menuReducer,
-  language: languageReducer
+  language: languageReducer,
+  social: socialReducer
 };
