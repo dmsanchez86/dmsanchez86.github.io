bd57c787bc93de35263f45b6975f440f75e47529
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-12 16:49:11 -0500

    nuevo componente para los items de los proyectos

---
+++ src/app/components/item-project/item-project.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LanguageItemProjectsI } from 'src/app/interfaces/LanguageI';
+import { ProjectItemI } from 'src/app/interfaces/ProjectItemI';
+import { AppState } from 'src/app/store';
+
+@Component({
+  selector: 'app-item-project',
+  template: `
+    <div [attr.tabIndex]="this.i + 1">
+      <div class="project" style="height: 50vh;width: 98vw;">
+        <h1 class="title">
+          <a [href]="project?.url" target="_blank">{{ (language | async)[project?.key] }}</a>
+          <app-tools [project]="project" [url]="'projects'"></app-tools>
+        </h1>
+      </div>
+    </div>
+  `,
+})
+export class ItemProjectComponent implements OnInit {
+  @Input() project: ProjectItemI;
+  @Input() i: number;
+  @Input() key: string;
+  language: Observable<LanguageItemProjectsI> = this.store.select(
+    (state) => state.language.current[this.key]
+  );
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {}
+}
