0685a3395779ace19d1283fff93cab2ac3733176
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-12 16:26:23 -0500

    ajuste para texto en cargador de preview

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { ProjectItemI } from 'src/app/interfaces/ProjectItemI';
 import { AppState } from 'src/app/store';
 
@@ -12,6 +13,8 @@ export class PreviewComponent {
   @Input() project: ProjectItemI;
   validationLoad: boolean = false;
   validationFullscreen: boolean = false;
+
+  language: Observable<string> = this.store.select(state => state.language.current.loading);
 
   constructor(private store: Store<AppState>, public sanitizer: DomSanitizer) {}
 
