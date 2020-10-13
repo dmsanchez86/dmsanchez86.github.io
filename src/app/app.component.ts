f8d574f9156fc44ecd8c7c1929e569e7ec7f96df
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-13 00:06:37 -0500

    validacion en el app component para poder dispachar la accion de abrir el poup si existe el parametro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { GlobalService } from './services/global.service';
 import { AppState, AppStateLanguaje } from './store';
+import { mostrarPopup } from './store/actions/global';
 import { ChangeLanguage } from './store/actions/language';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   language: Observable<AppStateLanguaje> = this.store.select(state => state.language);
 
-  constructor(private global: GlobalService, private store: Store<AppState>){
+  constructor(
+    private global: GlobalService,
+    private store: Store<AppState>,
+    private router: Router
+  ){
+    if(localStorage.language){
+      this.store.dispatch(ChangeLanguage({ key: localStorage.language }));
+    }
+  }
+
+  ngOnInit(){
     setTimeout(() => {
       let atomLoader: HTMLElement = document.querySelector('#atomLoader');
       atomLoader.style.display = 'none';
     }, 300);
 
-    if(localStorage.language){
-      this.store.dispatch(ChangeLanguage({ key: localStorage.language }));
+    if (
+      this.router.url.indexOf('view=profile') ||
+      this.router.url.indexOf('#about')
+    ) {
+      localStorage.fromParent = true;
+
+      this.store.dispatch(mostrarPopup({ payload: true }));
     }
   }
 
