c0cf6a858829a270636c2670480c41ff58f68efe
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-13 20:44:05 -0500

    se agregaron proyectos nuevos en la data de portafolio

--- src/app/store/reducers/portafolio/dataPortafolio.ts
+++ src/app/store/reducers/portafolio/dataPortafolio.ts
@@ -2,6 +2,96 @@ import { ProjectItemI } from 'src/app/interfaces/ProjectItemI';
 
 export const dataPortafolio: ProjectItemI[] = [
   {
+    name: 'Krika Cosmetics',
+    url: 'http://krikacosmetics.com',
+    preview: true,
+    key: 'krika',
+    type: 'portafolio'
+  },
+  {
+    name: 'Librería Nacional',
+    url: 'http://librerianacional.com/',
+    preview: true,
+    key: 'libreria_nacional',
+    type: 'portafolio'
+  },
+  {
+    name: 'Energiteca',
+    url: 'http://energiteca.com/',
+    preview: true,
+    key: 'energiteca',
+    type: 'portafolio'
+  },
+  {
+    name: 'AseoYa',
+    url: 'http://aseoya.com/',
+    preview: true,
+    key: 'aseoya',
+    type: 'portafolio'
+  },
+  {
+    name: 'Existaya',
+    url: 'http://existaya.com/',
+    preview: true,
+    key: 'existaya',
+    type: 'portafolio'
+  },
+  {
+    name: 'STP Networks',
+    url: 'http://stpnetworks.com.co/',
+    preview: true,
+    key: 'stp',
+    type: 'portafolio'
+  },
+  {
+    name: 'Coomeva MP',
+    url: 'http://coomeva.medicina-prepagada.com/',
+    preview: true,
+    key: 'coomeva_mp',
+    type: 'portafolio'
+  },
+  {
+    name: 'Coomeva SAO',
+    url: 'https://cuidamostusonrisa.co',
+    preview: true,
+    key: 'coomeva_sao',
+    type: 'portafolio'
+  },
+  {
+    name: 'Coomeva Protegete',
+    url: 'https://teprotegemos.coomeva.com.co/',
+    preview: true,
+    key: 'coomeva_pro',
+    type: 'portafolio'
+  },
+  {
+    name: 'Libreria Nacional App',
+    url_play_store: 'https://play.google.com/store/apps/details?id=com.existaya.librerianacional&hl=es_419',
+    preview: false,
+    url_app_store: 'https://apps.apple.com/co/app/librer%C3%ADa-nacional/id1480707806',
+    key: 'libreria_nacional_app',
+    type: 'portafolio',
+    app: true
+  },
+  {
+    name: 'Energiteca App',
+    url_play_store: 'https://play.google.com/store/apps/details?id=com.existaya.cxapp&hl=en_US',
+    url_app_store: 'https://apps.apple.com/gb/app/energiteca/id1441881799',
+    preview: false,
+    key: 'energiteca_app',
+    type: 'portafolio',
+    app: true
+  },
+  {
+    name: 'AseoYa App',
+    url_play_store: 'https://play.google.com/store/apps/details?id=com.aseoya.app&hl=es_UY',
+    url_app_store: 'https://apps.apple.com/co/app/aseoya/id1322485369',
+    preview: false,
+    key: 'aseoya_app',
+    type: 'portafolio',
+    app: true
+  },
+  {
     name: 'GO finanzas',
     url: 'http://gofinanzas.com/',
     preview: true,
@@ -60,7 +150,7 @@ export const dataPortafolio: ProjectItemI[] = [
   {
     name: 'JurisQuiz',
     url: 'https://play.google.com/store/apps/details?id=com.zopp.jurisquiz',
-    url_app: 'https://play.google.com/store/apps/details?id=com.zopp.jurisquiz',
+    url_play_store: 'https://play.google.com/store/apps/details?id=com.zopp.jurisquiz',
     app: true,
     key: 'jurisquiz',
     type: 'portafolio'
@@ -68,7 +158,7 @@ export const dataPortafolio: ProjectItemI[] = [
   {
     name: 'Therapy Tips for',
     url: 'https://play.google.com/store/apps/details?id=com.zopp.artritis',
-    url_app: 'https://play.google.com/store/apps/details?id=com.zopp.artritis',
+    url_play_store: 'https://play.google.com/store/apps/details?id=com.zopp.artritis',
     app: true,
     key: 'therapy',
     type: 'portafolio'
