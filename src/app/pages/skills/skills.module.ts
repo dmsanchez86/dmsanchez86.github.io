038e0bed16b2e178ca751067d75f8ad2b4af2245
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-16 02:46:35 -0500

    validacion en componente para que funcione el detalle

--- src/app/pages/skills/skills.module.ts
+++ src/app/pages/skills/skills.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { SkillsRoutingModule } from './skills-routing.module';
 import { SkillsComponent } from './skills.component';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
-  declarations: [SkillsComponent],
+  declarations: [SkillsComponent, DetailComponent],
   imports: [
     CommonModule,
     SkillsRoutingModule,
     ComponentsModule
-  ]
+  ],
+  exports: [DetailComponent]
 })
 export class SkillsModule { }
