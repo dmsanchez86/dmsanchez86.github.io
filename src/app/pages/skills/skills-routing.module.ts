8b7e6138f026aee4741bff654fad1ee2abd35ef3
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-16 01:58:40 -0500

    ruta para el detalle de la habilidad

--- src/app/pages/skills/skills-routing.module.ts
+++ src/app/pages/skills/skills-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
 
 import { SkillsComponent } from './skills.component';
 
-const routes: Routes = [{ path: '', component: SkillsComponent }];
+const routes: Routes = [
+  { path: '', component: SkillsComponent },
+  { path: 'detail/:slug', component: DetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
