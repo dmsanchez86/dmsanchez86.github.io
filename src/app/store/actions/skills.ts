16561fe14623022895990acf55d1cbb591583fab
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-16 02:43:37 -0500

    accion para setear una habilidad

--- src/app/store/actions/skills.ts
+++ src/app/store/actions/skills.ts
@@ -3,4 +3,11 @@ import { LanguageItemProfileSkillsI } from 'src/app/interfaces/LanguageI';
 
 export const cargarSkills = createAction(
   '[SKILLS] Cargar',
-  props<{payload: LanguageItemProfileSkillsI[]}>());
+  props<{payload: LanguageItemProfileSkillsI[]}>()
+);
+
+export const setSkill = createAction(
+  '[SKILL] Set',
+  props<{ slug: string }>()
+);
+
