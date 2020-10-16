55ab48e99e6c207ed8e96753dc69a243bb0ec666
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-16 02:43:23 -0500

    reducer para setear una skill

--- src/app/store/reducers/skills/index.ts
+++ src/app/store/reducers/skills/index.ts
@@ -1,15 +1,20 @@
 import { createReducer, on } from '@ngrx/store';
 import { AppStateSkillsI } from '../..';
-import { cargarSkills } from '../../actions/skills';
+import { cargarSkills, setSkill } from '../../actions/skills';
 import { data } from './data';
 
 const initialState: AppStateSkillsI = {
   data,
+  current: null,
 };
 
 const _skillsReducer = createReducer(
   initialState,
-  on(cargarSkills, (state, action) => ({ ...state, data: action.payload }))
+  on(cargarSkills, (state, action) => ({ ...state, data: action.payload })),
+  on(setSkill, (state, action) => ({
+    ...state,
+    current: state.data.filter((item) => item.name === action.slug)[0],
+  }))
 );
 
 export function skillsReducer(state, action) {
