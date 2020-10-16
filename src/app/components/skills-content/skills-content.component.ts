4b5c224fc633e3a9e5b40d186449c71c4f488f13
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-16 01:48:02 -0500

    validaciones en el template skills content para mostrarlas completas

--- src/app/components/skills-content/skills-content.component.ts
+++ src/app/components/skills-content/skills-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { LanguageItemProfileSkillsI, LanguageItemSkillsI } from 'src/app/interfaces/LanguageI';
@@ -9,27 +9,51 @@ import { AppState } from 'src/app/store';
   template: `
     <div class="skills-wrap" *ngIf="skills | async as skills">
       <div>
-        <h1 class="skills-title">{{ (language | async)?.title }}</h1>
-        <div class="center">
-          <a [routerLink]="['/skills']" routerLinkActive="router-link-active"  class="skills-link">{{ (language | async)?.label_link }}</a>
+        <h1 class="skills-title" [ngClass]="{'skills-title-complete': complete}">{{ (language | async)?.title }}</h1>
+        <div class="center" *ngIf="!complete">
+          <a [routerLink]="['/skills']" (click)="cerrarModal()" routerLinkActive="router-link-active"  class="skills-link">{{ (language | async)?.label_link }}</a>
         </div>
       </div>
-      <div class="skills-body center">
+      <div class="skills-body center" [ngClass]="{'skills-body-complete': complete}">
         <ng-container *ngFor="let skill of skills; let i = index">
-          <div *ngIf="i < 10" class="skills-item center" [title]="skill.title">
-            <i [class]="skill.icon"></i>
-            <span>{{ skill.title }}</span>
-          </div>
+          <ng-container *ngIf="complete; else elseTemplate">
+            <div class="skills-item center" [ngClass]="{'skills-item-complete': complete}" [title]="skill.title">
+              <i [class]="skill.icon"></i>
+              <span>{{ skill.title }}</span>
+            </div>
+          </ng-container>
+          <ng-template #elseTemplate>
+            <div *ngIf="i < 10" class="skills-item center" [title]="skill.title">
+              <i [class]="skill.icon"></i>
+              <span>{{ skill.title }}</span>
+            </div>
+          </ng-template>
+
         </ng-container>
       </div>
     </div>
   `,
 })
 export class SkillsContentComponent implements OnInit {
+  @Input() complete: boolean;
+
   language: Observable<LanguageItemSkillsI> = this.store.select(state => state.language.current.skills);
   skills: Observable<LanguageItemProfileSkillsI[]> = this.store.select(state => state.skills.data);
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {}
+
+  cerrarModal(){
+    let body = document.body;
+    let popup = document.querySelector('.popup');
+    let profile = document.querySelector(`.profile_content_home`);
+
+    body.classList.remove('profile');
+    profile.classList.remove('scaleOut');
+    profile.classList.remove('scaleIn');
+    popup.classList.remove('active');
+
+    setTimeout(() => profile.classList.add('scaleIn'), 100);
+  }
 }
