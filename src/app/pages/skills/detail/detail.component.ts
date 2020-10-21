8337aa2b2104efb21f419735aad4dd892597817b
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-20 19:53:06 -0500

    validacion en el template del detalle para traducir los textos

--- src/app/pages/skills/detail/detail.component.ts
+++ src/app/pages/skills/detail/detail.component.ts
@@ -17,31 +17,34 @@ import { bodyAddClass, bodyRemoveClass, favicon } from 'src/environments/global_
       style="padding: 10vh 0 1rem;min-height: 100vh;"
     >
       <div class="skills-wrap" *ngIf="skill$ | async as skill">
-        <div class="skills-icon-wrap">
-          <i [class]="skill.icon"></i>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col l6 s12">
-              <a class="skills-item-link" [routerLink]="['/skills']">
-                <i class="fas fa-arrow-left"></i> {{ (language$ | async).global.back }}
-              </a>
-            </div>
-            <div class="col l6 s12">
-              <h1 class="skills-title-complete">{{ skill?.title }}</h1>
-            </div>
+        <ng-container *ngIf="language$ | async as language">
+          <div class="skills-icon-wrap">
+            <i [class]="skill.icon"></i>
           </div>
-          <div class="row">
-            <div class="col l6 offset-l6 s12">
-              <div class="skills-body">
-                <div
-                  class="skills-item skills-item-detail skills-item-detail"
-                  [innerHTML]="skill.description"
-                ></div>
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <a class="skills-item-link" [routerLink]="['/skills']">
+                  <i class="fas fa-arrow-left"></i>
+                  {{ language.global.back }}
+                </a>
+              </div>
+              <div class="col l6 s12">
+                <h1 class="skills-title-complete">{{ skill?.title }}</h1>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col l6 offset-l6 s12">
+                <div class="skills-body">
+                  <div
+                    class="skills-item skills-item-detail skills-item-detail"
+                    [innerHTML]="language.profile.skills[skill.key]"
+                  ></div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </ng-container>
       </div>
     </div>
   `,
