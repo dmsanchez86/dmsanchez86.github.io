c3f3505ec80b21b7f43ce4a2768d25da46c45be3
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-21 21:47:43 -0500

    se creo validacion para obetener el padding bottom

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { LanguageItemProfileI } from 'src/app/interfaces/LanguageI';
 import { AppState } from 'src/app/store';
+import { qs } from 'src/environments/global_functions';
 
 @Component({
   selector: 'app-popup',
@@ -10,10 +11,22 @@ import { AppState } from 'src/app/store';
 })
 export class PopupComponent implements OnInit {
   language: Observable<LanguageItemProfileI> = this.store.select(state => state.language.current.profile);
+  show_popup: Observable<Boolean> = this.store.select(state => state.global.popup);
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.calcularVariablesCSS();
+  }
+
+  calcularVariablesCSS(){
+    setTimeout(() => {
+      let pBody = qs('.popup-body');
+      let pBodySocial = qs('.popup-body .project');
+      let h = (pBodySocial.getBoundingClientRect().height);
+
+      pBody['style'].setProperty('--pb-popup', `${h + 20}px`);
+    }, 100);
   }
 
 }
