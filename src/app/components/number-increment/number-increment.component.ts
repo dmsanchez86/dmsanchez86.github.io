32238b5466bc0ebb2acdc8edb5fcce4b9ecac229
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-10-22 23:36:59 -0500

    reload function in number increment component

--- src/app/components/number-increment/number-increment.component.ts
+++ src/app/components/number-increment/number-increment.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-number-increment',
-  template: `{{ initial }}`
+  template: `<span (click)="play(true)">{{ initial }}</span>`,
 })
 export class NumberIncrementComponent implements OnInit {
   @Input() num: number;
@@ -10,13 +10,20 @@ export class NumberIncrementComponent implements OnInit {
   initial: number = 0;
 
   ngOnInit() {
-    this.interval = setInterval(()=>{
-      if(this.initial < this.num){
-        this.initial++;
-      }else{
-        clearInterval(this.interval);
-      }
-    }, this.num < 10 ? 100 : 30);
+    this.play();
   }
 
+  play(reload?){
+    if(reload){
+      this.initial = 0;
+    }
+
+    this.interval = setInterval(() => {
+      if (this.initial < this.num) {
+        this.initial++;
+      } else {
+        clearInterval(this.interval);
+      }
+    }, this.num < 10 ? 100 : 15);
+  }
 }
