29a0a23c38529a1a48679e750b477844d4e34774
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-11-03 20:27:53 -0500

    se  ajusto modulo de 404

--- src/app/pages/page404/page404.module.ts
+++ src/app/pages/page404/page404.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Page404Component } from './page404.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ComponentsModule } from 'src/app/components/components.module';
 
+const routes: Routes = [
+  { path: '', component: Page404Component }
+];
 
 @NgModule({
   declarations: [Page404Component],
-  imports: [ CommonModule ]
+  imports: [ 
+    CommonModule,
+    ComponentsModule,
+    RouterModule.forChild(routes)
+  ]
 })
 export class Page404Module { }
