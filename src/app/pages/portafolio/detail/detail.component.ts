057af79130b5079f5ed6d54052c64eae3d172be2
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-11-05 20:46:26 -0500

    ajustes en el detalle del portafolio para que cargue y cambie el color del tema

--- src/app/pages/portafolio/detail/detail.component.ts
+++ src/app/pages/portafolio/detail/detail.component.ts
@@ -1,37 +1,69 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { LanguageItemPortafolioI } from 'src/app/interfaces/LanguageI';
 import { ProjectItemI } from 'src/app/interfaces/ProjectItemI';
+import { GlobalService } from 'src/app/services/global.service';
 import { AppState } from 'src/app/store';
 import { resetPortafolio, setPortafolio } from 'src/app/store/actions/portafolio';
-import { bodyAddClass, bodyRemoveClass } from 'src/environments/global_functions';
+import { bodyAddClass, bodyRemoveClass, removeThemeTMP } from 'src/environments/global_functions';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
 })
 export class DetailComponent implements OnInit, OnDestroy {
-
   project: Observable<ProjectItemI> = this.store.select(state => state.portafolio.current);
   language: Observable<LanguageItemPortafolioI> = this.store.select(state => state.language.current.portafolio);
 
-  constructor(private store: Store<AppState>) {
-    if(localStorage.portafolio && !localStorage.fromTools) {
-      this.store.dispatch(setPortafolio(JSON.parse(localStorage.portafolio)));
-    }else{
-      if(localStorage.fromTools){
-        delete localStorage.fromTools;
+  name: string;
+  sus: Subscription = new Subscription();
+
+  constructor(
+    private store: Store<AppState>,
+    private route: ActivatedRoute,
+    private global: GlobalService,
+    private actions: Actions
+  ) {
+    this.name = this.route.snapshot.params.slug;
+
+    this.route.params.subscribe(params => {
+      if (params.slug !== this.name) {
+        setTimeout(() => this.setItem(), 50);
       }
+
+      this.name = params.slug;
+    });
+
+    this.sus = this.actions.subscribe((action: any) => {
+      console.log(action);
+      if (action.type === setPortafolio.type) {
+        this.global.metaColorP(null, action.slug);
+      }
+    });
+  }
+
+  setItem() {
+    if (this.name) {
+      this.store.dispatch(setPortafolio({ slug: this.name }));
     }
   }
 
   ngOnInit(): void {
     bodyAddClass('portafolio');
+
+    this.global.titlePage(`portafolio`, this.name);
+    
+    this.setItem();
   }
+
   ngOnDestroy(): void {
     this.store.dispatch(resetPortafolio());
     bodyRemoveClass('portafolio');
+    removeThemeTMP();
+    this.sus.unsubscribe();
   }
 
 }
