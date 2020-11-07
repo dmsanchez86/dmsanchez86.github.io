cb19258e370ba52a9869a0d3c157198dc20dae6c
Daniel M Sanchez <dmsanchez86@misena.edu.co>
2020-11-06 21:55:50 -0500

    se movio html de language content a file

--- src/app/components/language-content/language-content.component.ts
+++ src/app/components/language-content/language-content.component.ts
@@ -7,35 +7,7 @@ import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-language-content',
-  template: `
-    <div
-      class="language-content"
-      [attr.title-ref]="language.current.label"
-      *ngIf="language | async as language">
-      <span>
-        <a
-          href="javascript:void(0)"
-          [ngClass]="{ active: language.current.ref === 'es' }"
-          [attr.title]="language.current.label"
-          (click)="cambiarLenguaje('es')"
-        >
-          <span><i class="fa fa-language"></i></span>
-          {{ language.es.ref }}
-        </a>
-      </span>
-      <span>
-        <a
-          href="javascript::void(0)"
-          [ngClass]="{ active: language.current.ref === 'en' }"
-          [attr.title]="language.current.label"
-          (click)="cambiarLenguaje('en')"
-        >
-          <span><i class="fa fa-language"></i></span>
-          {{ language.en.ref }}
-        </a>
-      </span>
-    </div>
-  `,
+  templateUrl: './language-content.component.html',
 })
 export class LanguageContentComponent {
   language: Observable<AppStateLanguaje> = this.store.select((state) => state.language);
